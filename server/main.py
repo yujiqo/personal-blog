d97c29fb0ffc032fd94499a41eb4cc10a4bf601a
yujiqo <danilastolyarov97@gmail.com>
2025-12-05 21:06:47 +0500

    allowed * in cors

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from .db import UPLOAD_DIR, engine
@@ -15,5 +16,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
 app.include_router(posts.router)
